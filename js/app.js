96e10bde60630387df035016039c9cbd00f381c4
May Miller-Ricci <maymillerricci@gmail.com>
2016-09-06 21:35:42 -0700

    build search results html and show on page

--- js/app.js
+++ js/app.js
@@ -12,7 +12,26 @@ function performSearch(searchTerm) {
   $.ajax(url, {
     data: data,
     success: function(response) {
-      console.log(response);
+      showSearchResults(response);
     }
   }); 
+}
+
+function showSearchResults(response) {
+  var moviesHtml = "";
+  for (var i = 0; i < response["Search"].length; i++) {
+    moviesHtml += buildMovieListItem(response["Search"][i]);
+  }
+  $("#movies").html(moviesHtml);
+}
+
+function buildMovieListItem(movieInfo) {
+  var movieListItem = "<li><div class='poster-wrap'><img class='movie-poster' src='";
+  movieListItem += movieInfo["Poster"];
+  movieListItem += "'></div><span class='movie-title'>";
+  movieListItem += movieInfo["Title"];
+  movieListItem += "</span><span class='movie-year'>";
+  movieListItem += movieInfo["Year"];
+  movieListItem += "</span></li>";
+  return movieListItem;
 }
